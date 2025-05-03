4522315ae826ce4caa22eb66a8403b41ec28f410
zark <zarkalhs@gmail.com>
2025-05-03 20:57:52 +0300

    Push foundry project to contracts and cargo project to relayer

---
+++ relayer/target/debug/deps/relayer-df9b9f319e798b8d.d
@@ -0,0 +1,5 @@
+/Users/zark/Code/build/infra/erc20-x/relayer/target/debug/deps/relayer-df9b9f319e798b8d: src/main.rs
+
+/Users/zark/Code/build/infra/erc20-x/relayer/target/debug/deps/relayer-df9b9f319e798b8d.d: src/main.rs
+
+src/main.rs:
