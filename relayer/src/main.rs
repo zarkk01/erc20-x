088c689bd448ce06fcfd495480a67dffe372ebd9
zark <zarkalhs@gmail.com>
2025-05-04 23:36:57 +0300

    Add some env examples with anvil keys and irrelevant rpc urls

--- relayer/src/main.rs
+++ relayer/src/main.rs
@@ -17,33 +17,27 @@ async fn main() -> anyhow::Result<()> {
     // 2. Set everything for L2 A
     let src_ws_url = env::var("SRC_RPC_WS")?;
     let src_contract_address: Address = env::var("SRC_CONTRACT_ADDR")?.parse()?;
-
     let src_ws = Ws::connect(src_ws_url).await?;
     let src_provider = Provider::new(src_ws);
     let src_client = Arc::new(src_provider.clone());
-
     let src_private_key = env::var("SRC_PRIVATE_KEY")?;
     let src_wallet: LocalWallet = src_private_key
         .parse::<LocalWallet>()?
         .with_chain_id(31337u64); // same Id, don't mind for now
     let src_signer = Arc::new(SignerMiddleware::new(src_provider, src_wallet.clone()));
-
     let src_contract = ZarkToken::new(src_contract_address, src_client.clone());
 
     // 2. Set everything for L2 B
     let dst_ws_url = env::var("DST_RPC_WS")?;
     let dst_contract_address: Address = env::var("DST_CONTRACT_ADDR")?.parse()?;
-
     let dst_ws = Ws::connect(dst_ws_url).await?;
     let dst_provider = Provider::new(dst_ws);
     let dst_client = Arc::new(dst_provider.clone());
-
     let dst_private_key = env::var("DST_PRIVATE_KEY")?;
     let dst_wallet: LocalWallet = dst_private_key
         .parse::<LocalWallet>()?
         .with_chain_id(31337u64); // same Id, don't mind for now
     let dst_signer = Arc::new(SignerMiddleware::new(dst_provider, dst_wallet.clone()));
-
     let dst_contract = ZarkToken::new(dst_contract_address, dst_signer.clone());
 
     // 3. Set up the event stream
